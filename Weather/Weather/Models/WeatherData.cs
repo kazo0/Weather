09fc533fa85d669bb5ac7b7eb5394595d92995da
Steve Bilogan <steve.bilogan@gmail.com>
2020-07-26 15:14:45 -0400

    Add project files.

---
+++ Weather/Weather/Models/WeatherData.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace Weather.Models
+{
+    public class WeatherData
+    {
+        [JsonProperty("cod")]
+        public string Cod { get; set; }
+
+        [JsonProperty("message")]
+        public long Message { get; set; }
+
+        [JsonProperty("cnt")]
+        public long Cnt { get; set; }
+
+        [JsonProperty("list")]
+        public List[] List { get; set; }
+
+        [JsonProperty("city")]
+        public City City { get; set; }
+    }
+
+    public class City
+    {
+        [JsonProperty("id")]
+        public long Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("coord")]
+        public Coord Coord { get; set; }
+
+        [JsonProperty("country")]
+        public string Country { get; set; }
+
+        [JsonProperty("population")]
+        public long Population { get; set; }
+
+        [JsonProperty("timezone")]
+        public long Timezone { get; set; }
+
+        [JsonProperty("sunrise")]
+        public long Sunrise { get; set; }
+
+        [JsonProperty("sunset")]
+        public long Sunset { get; set; }
+    }
+
+    public class Coord
+    {
+        [JsonProperty("lat")]
+        public double Lat { get; set; }
+
+        [JsonProperty("lon")]
+        public double Lon { get; set; }
+    }
+
+    public class List
+    {
+        [JsonProperty("dt")]
+        public long Dt { get; set; }
+
+        [JsonProperty("main")]
+        public MainClass Main { get; set; }
+
+        [JsonProperty("weather")]
+        public Weather[] Weather { get; set; }
+
+        [JsonProperty("clouds")]
+        public Clouds Clouds { get; set; }
+
+        [JsonProperty("wind")]
+        public Wind Wind { get; set; }
+
+        [JsonProperty("visibility")]
+        public long Visibility { get; set; }
+
+        [JsonProperty("pop")]
+        public double Pop { get; set; }
+
+        [JsonProperty("sys")]
+        public Sys Sys { get; set; }
+
+        [JsonProperty("dt_txt")]
+        public DateTimeOffset DtTxt { get; set; }
+
+        [JsonProperty("rain", NullValueHandling = NullValueHandling.Ignore)]
+        public Rain Rain { get; set; }
+    }
+
+    public class Clouds
+    {
+        [JsonProperty("all")]
+        public long All { get; set; }
+    }
+
+    public class MainClass
+    {
+        [JsonProperty("temp")]
+        public double Temp { get; set; }
+
+        [JsonProperty("feels_like")]
+        public double FeelsLike { get; set; }
+
+        [JsonProperty("temp_min")]
+        public double TempMin { get; set; }
+
+        [JsonProperty("temp_max")]
+        public double TempMax { get; set; }
+
+        [JsonProperty("pressure")]
+        public long Pressure { get; set; }
+
+        [JsonProperty("sea_level")]
+        public long SeaLevel { get; set; }
+
+        [JsonProperty("grnd_level")]
+        public long GrndLevel { get; set; }
+
+        [JsonProperty("humidity")]
+        public long Humidity { get; set; }
+
+        [JsonProperty("temp_kf")]
+        public double TempKf { get; set; }
+    }
+
+    public class Rain
+    {
+        [JsonProperty("3h")]
+        public double The3H { get; set; }
+    }
+
+    public class Sys
+    {
+        [JsonProperty("pod")]
+        public string Pod { get; set; }
+    }
+
+    public class Weather
+    {
+        [JsonProperty("id")]
+        public long Id { get; set; }
+
+        [JsonProperty("main")]
+        public string Main { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        [JsonProperty("icon")]
+        public string Icon { get; set; }
+    }
+
+    public class Wind
+    {
+        [JsonProperty("speed")]
+        public double Speed { get; set; }
+
+        [JsonProperty("deg")]
+        public long Deg { get; set; }
+    }
+}
---
+++ Weather/Weather/Services/OpenWeatherMapWeatherService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Threading.Tasks;
+using Weather.Models;
+
+namespace Weather.Services
+{
+	public class OpenWeatherMapWeatherService : IWeatherService
+	{
+		public async Task<Forecast> GetForecast(double latitude, double longitude)
+		{
+			var language = CultureInfo.CurrentCulture.TwoLetterISOLanguageName;
+
+		}
+	}
+}
